9115caa0c83351da97b83ea79379357b0f2350f1
hezhongfeng <287645587@qq.com>
2020-07-31 11:24:40 +0800

    滑动窗口

---
+++ js/算法/无重复字符的最长子串/code.js
@@ -0,0 +1,31 @@
+/*
+ * @lc app=leetcode.cn id=3 lang=javascript
+ *
+ * [3] 无重复字符的最长子串
+ */
+
+// @lc code=start
+/**
+ * @param {string} s
+ * @return {number}
+ */
+var lengthOfLongestSubstring = function (s) {
+  let left = 0;
+  let right = 0;
+  let obj = {};
+  let max = 0;
+  for (; right < s.length; right++) {
+    if (obj[s[right]] === undefined) {
+      obj[s[right]] = 0;
+    }
+    obj[s[right]]++;
+    while (obj[s[right]] > 1) {
+      obj[s[left]]--;
+      left++;
+    }
+    max = Math.max(max, right - left + 1);
+  }
+  return max;
+};
+
+// @lc code=end
